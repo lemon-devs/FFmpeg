bb9d27749aedaa097e51bdf86e84d3ac421debac
Asd-g <65298684+Asd-g@users.noreply.github.com>
2023-11-17 12:11:04 +0200

    Revert "lavc/decode: do not perform decoding when sending draining packets"
    This reverts commit b54603a26a53bfd89fd19b97308e64f4fbeef4b3.
    
    https://github.com/HomeOfAviSynthPlusEvolution/L-SMASH-Works/issues/ 48

--- libavcodec/decode.c
+++ libavcodec/decode.c
@@ -732,8 +732,9 @@ int attribute_align_arg avcodec_send_packet(AVCodecContext *avctx, const AVPacke
     } else
         dc->draining_started = 1;
 
-    if (!avci->buffer_frame->buf[0] && !dc->draining_started) {
+    if (!avci->buffer_frame->buf[0]) {
         ret = decode_receive_frame_internal(avctx, avci->buffer_frame, 0);
+
         if (ret < 0 && ret != AVERROR(EAGAIN) && ret != AVERROR_EOF)
             return ret;
     }
